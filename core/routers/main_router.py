b8903aa17f4eb9831a2c21c95b9a8b178609b79c
daniil_gyrman0909 <gyrman01app@mail.ru>
2024-12-01 19:36:30 +0300

    fix

--- core/routers/main_router.py
+++ core/routers/main_router.py
@@ -16,14 +16,14 @@ async def start(mes: Message, state: FSMContext):
     await mes.delete()
 
 
-@router.message(Command("test"))
-async def test(mes: Message):
-    # await mes.answer(text=END_REG_MSG_2, reply_markup=instruction_2.as_markup())
-    await mes.answer_video(video="BAACAgIAAxkBAAICQ2dMj5NSDDcG9Tyz6bj7Ofu0FYsPAAKoZgAC-XloSgPtw2y8dmBeNgQ")
-    ...
-
-
-@router.message(F.video)
-def test2(msg: Message):
-    print("test2")
-    print(msg.video.file_id)
+# @router.message(Command("test"))
+# async def test(mes: Message):
+#     # await mes.answer(text=END_REG_MSG_2, reply_markup=instruction_2.as_markup())
+#     await mes.answer_video(video="BAACAgIAAxkBAAICQ2dMj5NSDDcG9Tyz6bj7Ofu0FYsPAAKoZgAC-XloSgPtw2y8dmBeNgQ")
+#     ...
+#
+#
+# @router.message(F.video)
+# def test2(msg: Message):
+#     print("test2")
+#     print(msg.video.file_id)
